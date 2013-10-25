349f855013ba2b5030f37f564aeae8dafe8311cd
Panu Ervamaa <panu.ervamaa@frantic.com>
2013-10-25 17:49:16 +0300

    cleanup start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-# ./start.sh
-# ./start.sh halsua.of.frantic.com:3000
 export CLEARDB_DATABASE_URL=mysql://root:@localhost/DATABASENAME
 export WP_CACHE=true
 export MEMCACHIER_SERVERS=localhost:11211
