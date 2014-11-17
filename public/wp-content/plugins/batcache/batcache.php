80b20bca161510f619ad1b29b360109ccaa38260
Tuomas Arokanto <tuomas.arokanto@frantic.com>
2014-11-17 16:59:20 +0200

    Add support for SSL to Batcache Manager

--- public/wp-content/plugins/batcache/batcache.php
+++ public/wp-content/plugins/batcache/batcache.php
@@ -2,10 +2,10 @@
 /*
 Plugin name: Batcache Manager
 Plugin URI: http://wordpress.org/extend/plugins/batcache/
-Description: This optional plugin improves Batcache.
+Description: This optional plugin improves Batcache. Modified by Frantic to support SSL admin.
 Author: Andy Skelton
 Author URI: http://andyskelton.com/
-Version: 1.2
+Version: 1.2.1
 */
 
 // Do not load if our advanced-cache.php isn't loaded
@@ -29,9 +29,22 @@ function batcache_post($post_id) {
 	if ( $post->post_type == 'revision' || get_post_status($post_id) != 'publish' )
 		return;
 
-	batcache_clear_url( get_option('home') );
-	batcache_clear_url( trailingslashit( get_option('home') ) );
-	batcache_clear_url( get_permalink($post_id) );
+	$home_url = get_option('home');
+	$home_url_trailing_slash = trailingslashit( get_option('home') );
+	$permalink = get_permalink($post_id);
+
+	batcache_clear_url( $home_url );
+	batcache_clear_url( $home_url_trailing_slash );
+	batcache_clear_url( $permalink );
+
+	if ($batcache->is_ssl()) {
+		// If the site uses SSL, for some reason the pages get stored to MemCachier as http.
+		// This means that the we need to replace SSL URL's with normal http in order for the
+		// caches to clear properly.
+		batcache_clear_url( str_replace('https://', 'http://', $home_url) );
+		batcache_clear_url( str_replace('https://', 'http://', $home_url_trailing_slash) );
+		batcache_clear_url( str_replace('https://', 'http://', $permalink) );
+	}
 }
 
 function batcache_clear_url($url) {
